510f1aa347c6e986f20d9967ea46a9512e715604
andrewkvavlewhite <andrewkvavlewhite@gmail.vom>
2020-07-23 13:54:25 -0700

    cleanup

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -66,36 +66,6 @@ const App = ( props: Props ) => {
 	// const [ appointmentMap, setAppointmentMap ] = useState({});
 	const { isMobile } = useMedia();
 
-
-
-	const callApi = async () => {
-		const response = await fetch('/api/hello');
-		const body = await response.json();
-		if (response.status !== 200) throw Error(body.message);
-		return body;
-	  };
-	const handleSubmit = async e => {
-		e.preventDefault();
-		const response = await fetch('/api/world', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ post: this.state.post }),
-		});
-		const body = await response.text();
-		// this.setState({ responseToPost: body });
-		console.log(body)
-	};
-
-	useEffect(() => {
-		callApi()
-			.then(res => console.log(res.express))
-			.catch(err => console.log(err));
-	}, []);
-
-
-
 	const month = date.toLocaleString( 'en-us', { month: isMobile ? 'short' : 'long' } );
 	const year = dateFns.getYear( date );
 
